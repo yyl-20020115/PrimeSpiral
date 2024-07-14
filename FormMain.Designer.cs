a1bcde4adcd3f12e81fbf0836b5f430f6242a1f5
Yilin Yang <yyl_20050115@hotmail.com>
2024-07-15 05:32:13 +0800

    添加项目文件。

---
+++ FormMain.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace PrimeSpiral
+{
+    partial class FormMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox = new PictureBox();
+            buttonGenerate = new Button();
+            panelScroll = new Panel();
+            ((System.ComponentModel.ISupportInitialize)pictureBox).BeginInit();
+            panelScroll.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            pictureBox.BackColor = Color.Yellow;
+            pictureBox.Dock = DockStyle.Fill;
+            pictureBox.Location = new Point(0, 0);
+            pictureBox.Name = "pictureBox";
+            pictureBox.Size = new Size(888, 554);
+            pictureBox.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox.TabIndex = 0;
+            pictureBox.TabStop = false;
+            // 
+            // buttonGenerate
+            // 
+            buttonGenerate.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            buttonGenerate.Location = new Point(910, 537);
+            buttonGenerate.Name = "buttonGenerate";
+            buttonGenerate.Size = new Size(75, 23);
+            buttonGenerate.TabIndex = 1;
+            buttonGenerate.Text = "Generate";
+            buttonGenerate.UseVisualStyleBackColor = true;
+            buttonGenerate.Click += ButtonGenerate_Click;
+            // 
+            // panelScroll
+            // 
+            panelScroll.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panelScroll.AutoScroll = true;
+            panelScroll.BorderStyle = BorderStyle.Fixed3D;
+            panelScroll.Controls.Add(pictureBox);
+            panelScroll.Location = new Point(12, 12);
+            panelScroll.Name = "panelScroll";
+            panelScroll.Size = new Size(892, 558);
+            panelScroll.TabIndex = 2;
+            // 
+            // FormMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1033, 582);
+            Controls.Add(panelScroll);
+            Controls.Add(buttonGenerate);
+            Name = "FormMain";
+            Text = "Prime Spiral";
+            ((System.ComponentModel.ISupportInitialize)pictureBox).EndInit();
+            panelScroll.ResumeLayout(false);
+            panelScroll.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox;
+        private Button buttonGenerate;
+        private Panel panelScroll;
+    }
+}
---
+++ FormMain.cs
@@ -0,0 +1,87 @@
+namespace PrimeSpiral;
+
+public partial class FormMain : Form
+{
+    public FormMain()
+    {
+        InitializeComponent();
+    }
+
+    public enum Directions : uint
+    {
+        Right = 0,
+        Down = 1,
+        Left = 2,
+        Up = 3,
+    }
+    public static int DirectionsCount = Enum.GetValues(typeof(Directions)).Length;
+    public static Point Step(Point p, Directions direction) => direction switch
+    {
+        Directions.Left => new Point(p.X - 1, p.Y + 0),
+        Directions.Right => new Point(p.X + 1, p.Y + 0),
+        Directions.Up => new Point(p.X + 0, p.Y - 1),
+        Directions.Down => new Point(p.X + 0, p.Y + 1),
+        _ => p,
+    };
+    public static Directions MakeTurn(Directions direction)
+        => (Directions)(((uint)direction + 1) % DirectionsCount);
+    public static int GetRatioPart(int r, int max, int range = 255)
+        => (int)Math.Round((1.0 - r / (double)max) * range);
+
+    public static bool IsPrime(long n)
+    {
+        if (n <= 1) return false;
+        for (long i = 2; i * i <= n; i++)
+        {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+    private void ButtonGenerate_Click(object sender, EventArgs e)
+    {
+        using var writer = new StreamWriter("Points.txt");
+        var bitmap = new Bitmap(this.pictureBox.Width, this.pictureBox.Height);
+        var cp = new Point(bitmap.Width / 2, bitmap.Height / 2);
+        int max_count = bitmap.Height * bitmap.Height;
+        int point_count = 0;
+        //Center Point First
+        int r = 1;
+        int min_r = Math.Max(bitmap.Width, bitmap.Height);
+        long n = 0;
+        var direction = Directions.Up;
+        do
+        {
+            for (uint c = 0; c < 2; c++)
+            {
+                direction = MakeTurn(direction);
+
+                for (uint i = 0; i < r; i++)
+                {
+                    var prime = IsPrime(n);
+                    writer.WriteLine($"({cp.X},{cp.Y})={n},{(prime?1:0)}");
+                    cp = Step(cp, direction);
+                    n++;
+                    if (cp.X >= 0 && cp.Y >= 0 && cp.X < bitmap.Width && cp.Y < bitmap.Height)
+                    {
+                        if (prime)
+                        {
+                            bitmap.SetPixel(cp.X, cp.Y, Color.Blue);
+                        }
+                        else
+                        {
+                            bitmap.SetPixel(cp.X, cp.Y, Color.FromArgb(
+                                GetRatioPart(r, min_r),
+                                GetRatioPart(r, min_r),
+                                GetRatioPart(r, min_r)));
+                        }
+                        point_count++;
+                    }
+                }
+            }
+            r++;
+
+        } while (point_count < max_count);
+
+        this.pictureBox.Image = bitmap;
+    }
+}
